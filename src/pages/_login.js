76a19b3b2098653137dab3b1475d919234251198
Jimmy2038 <119540791+Jimmy2038@users.noreply.github.com>
2024-01-28 13:44:01 +0100

    Update _login.js

--- src/pages/_login.js
+++ src/pages/_login.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, {useState } from "react";
 import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [email, setEmail] = useState("admin@gmail.com");
